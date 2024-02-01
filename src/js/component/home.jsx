aca9ddfcb8b2ee9b958a0b787e98e5c1e1c3c18e
Robert Bartlett <39223283+Newux@users.noreply.github.com>
2024-02-01 03:40:25 +0000

    initial commit, created navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,35 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import oneAgent from "../../img/oneagent logo 2024.png";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="body" style={{background: "black", height : "700px"}}>
+			<nav className="navbar bg-body-tertiary">
+				<div className="container" style={{background: "black"}}>
+					<a className="navbar-brand" href="#">
+						<img src={oneAgent} alt="Bootstrap" width="200" height="86"/>
+					</a>
+				</div>
+			</nav>
+			<div className="text-center">
+				<h1 className="text-center mt-5">Hello Rigo!</h1>
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					Yaaaaaaasssssssss
+				</a>
+				<p>
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+					love!
+				</p>
+			</div>
 		</div>
+
 	);
 };
 
