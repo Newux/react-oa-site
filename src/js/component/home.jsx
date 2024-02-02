6be7f04ad5306817ca225e7005504a3d812fd157
Robert Bartlett <39223283+Newux@users.noreply.github.com>
2024-02-02 05:05:55 +0000

    completed top section of home page, continue to second section

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import warehouseImage from "../../img/WHM.png";
 import oneAgent from "../../img/oneagent logo 2024.png";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="body" style={{background: "black", height : "700px"}}>
-			<nav className="navbar bg-body-tertiary">
-				<div className="container" style={{background: "black"}}>
+		<div className="body" style={{ background: "#1e1e1e", height: "700px" }}>
+			<nav className="container navbar bg-body-tertiary" style={{ background: "#1e1e1e" }}>
+				<div className="container">
 					<a className="navbar-brand" href="#">
-						<img src={oneAgent} alt="Bootstrap" width="200" height="86"/>
+						<img src={oneAgent} alt="Bootstrap" width="200" height="86" style={{ marginLeft: '95px' }} />
 					</a>
 				</div>
 			</nav>
-			<div className="text-center">
-				<h1 className="text-center mt-5">Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					Yaaaaaaasssssssss
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-					love!
-				</p>
+			<div className="d-flex" style={{ marginLeft: '180px', marginTop: '50px' }}>
+				<img src={warehouseImage} width="600" />
+				<div className="message" style={{ marginLeft: '50px' }}>
+					<div className="first section">
+						<p className="heading-one" style={{ color: '#4ca1f9' }}>WAREHOUSING, CUSTOMER PORTAL AND BROKERAGE IN ONE</p>
+						<h1 className="heading-two"
+							style={{
+								color: 'white',
+								width: '350px',
+								font: '100px'
+							}}>
+							Turn key courier solution
+						</h1>
+						<p className="pitch" style={{ width: '450px', color: 'white', font: '12px', fontWeight: '300', marginTop: '20px' }}>
+							At One Agent, we developed a solution that puts the control
+							back in your hands. Our warehousing and technology
+							allows you to focus on the final mile while adding value for
+							your customers.
+						</p>
+					</div>
+					<a href="#" className="btn"
+						style={{
+							background: '#5257c6',
+							color: 'white',
+							width: '170px',
+							height: '40px',
+							marginTop: '30px',
+							borderRadius: '2px',
+							boxShadow: '0 1px 3px rgba(1,1,1,0.5)'
+						}}>
+						Contact Us
+					</a>
+				</div>
 			</div>
+
+
 		</div>
 
 	);
